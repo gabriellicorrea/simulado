21bb8f19e578f5af4a8c67ab47941176bcb0ddcc
Gabrielli <gabriellicorrea7@gmail.com>
2019-06-17 13:55:26 +0000

    Ex 1 resolvido - validando email

--- exercicio1.js
+++ exercicio1.js
@@ -1,1 +1,23 @@
-alert("oi")
+function validaEmail(str){
+    var padrao = /[a-z0-9]+@[a-z0-9]+\.[a-z0-9]{2,}(\.[a-z0-9]{2,})?/;
+    
+     if(padrao.test(str)==true){
+        alert("Email valido ")
+    }
+    else{
+        alert("Email invalido")
+    }
+    return padrao.test(str);
+   
+}
+
+
+function teste(){
+    document.write(validaEmail("gabrielli@fatec.com.br"));
+    
+}
+
+
+
+/*Um e-mail  ́e v ́alido se este possuir pelo menos um @ e pelo menos um ponto ap ́os dois caract ́eres posteriores.
+Implemente uma fun ̧c ̃ao que receba por parˆametro uma string e retorne se esta  ́e um e-mail v ́alido ou n ̃ao.*/
